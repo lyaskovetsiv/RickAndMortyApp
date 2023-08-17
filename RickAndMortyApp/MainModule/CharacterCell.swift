d8b125ec7c6ef7081e777df1c7fb2037db961927
Ivan Lyaskovets <lyaskovets.iv@gmail.com>
2023-08-17 17:38:03 +0300

    Added CharacterCell

---
+++ RickAndMortyApp/MainModule/CharacterCell.swift
@@ -0,0 +1,76 @@
+//
+//  CharacterCell.swift
+//  RickAndMortyApp
+//
+//  Created by Иван Лясковец on 17.08.2023.
+//
+
+import UIKit
+
+final class CharacterCell: UICollectionViewCell {
+
+	static let identifier: String = "characterCell"
+
+	private var photoImageView: UIImageView = {
+		let view = UIImageView(frame: .zero)
+		view.backgroundColor = .yellow
+		view.layer.cornerRadius = 10
+		return view
+	}()
+
+	private var nameLabel: UILabel = {
+		let view = UILabel(frame: .zero)
+		view.tintColor = .white
+		view.font = .systemFont(ofSize: 15)
+		view.textAlignment = .center
+		view.numberOfLines = 1
+		return view
+	}()
+
+	// MARK: - Inits
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupView()
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Public methods
+
+	public func configureCell(with model: String) {
+		nameLabel.text = model
+	}
+}
+
+// MARK: - Private methods
+
+extension CharacterCell {
+	private func setupView() {
+		backgroundColor = #colorLiteral(red: 0.1501607001, green: 0.1651832163, blue: 0.2201651633, alpha: 1)
+		layer.cornerRadius = 16
+		addSubview(photoImageView)
+		addSubview(nameLabel)
+		setupConstraits()
+	}
+
+	private func setupConstraits() {
+		photoImageView.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			photoImageView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+			photoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+			photoImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+			photoImageView.heightAnchor.constraint(equalToConstant: 140)
+		])
+
+		nameLabel.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+			nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+			nameLabel.topAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: 16),
+			nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16)
+		])
+	}
+}
--- RickAndMortyApp/MainModule/MainViewController.swift
+++ RickAndMortyApp/MainModule/MainViewController.swift
@@ -13,9 +13,14 @@ final class MainViewController: UIViewController {
 	private var mainCollectionView: UICollectionView = {
 		let layout = UICollectionViewFlowLayout()
 		let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
-		view.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+		view.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+		view.backgroundColor = .red
+		view.showsVerticalScrollIndicator = false
+		view.register(CharacterCell.self, forCellWithReuseIdentifier: CharacterCell.identifier)
 		return view
 	}()
+
+	private var characters: [String] = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Beth Smith", "Rick Smith", "Morty Smith", "Summer Smith", "Beth Smith", "Rick Smith"]
 
 	// MARK: - LifeCycleOfVC
 
@@ -33,6 +38,9 @@ extension MainViewController {
 		view.backgroundColor = #colorLiteral(red: 0.01332889963, green: 0.04810451716, blue: 0.1187042817, alpha: 1)
 		navigationController?.navigationBar.prefersLargeTitles = true
 
+		mainCollectionView.dataSource = self
+		mainCollectionView.delegate = self
+
 		view.addSubview(mainCollectionView)
 		setupConstraits()
 	}
@@ -45,5 +53,43 @@ extension MainViewController {
 			mainCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
 			mainCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
 		])
+	}
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension MainViewController: UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		characters.count
+	}
+
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CharacterCell.identifier, for: indexPath) as? CharacterCell else {
+			fatalError("Failed to load CharacterCell")
+		}
+		cell.configureCell(with: characters[indexPath.row])
+		return cell
+	}
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension MainViewController: UICollectionViewDelegate {
+
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		CGSize(width: 156, height: 202)
+	}
+
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+		16
+	}
+
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		16
 	}
 }
