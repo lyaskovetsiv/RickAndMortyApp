74c6174ac9ffe33b8273bc18acabbad1ca689693
Ivan Lyaskovets <lyaskovets.iv@gmail.com>
2023-08-17 16:27:00 +0300

    Initial commit

---
+++ RickAndMortyApp/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Иван Лясковец on 17.08.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		view.backgroundColor = .white
+	}
+}
+
