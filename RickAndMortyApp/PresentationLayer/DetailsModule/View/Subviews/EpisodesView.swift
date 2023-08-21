744f31e6cebbbe847a5c84d79e27feb0fdb43fa0
Ivan Lyaskovets <lyaskovets.iv@gmail.com>
2023-08-21 16:34:24 +0300

    Added progressView to SUI (for better UI)

--- RickAndMortyApp/PresentationLayer/DetailsModule/View/Subviews/EpisodesView.swift
+++ RickAndMortyApp/PresentationLayer/DetailsModule/View/Subviews/EpisodesView.swift
@@ -23,33 +23,38 @@ struct EpisodesView: View {
 	var body: some View {
 		VStack(spacing: 0) {
 			SectionHeader(text: "Episodes")
-			VStack (spacing: -24) {
-				ForEach(viewModel.episodes, id: \.name) { episode in
-					ZStack {
-						Color.groupColor
-						VStack {
-							HStack {
-								Text(episode.name)
-									.font(Font.custom("Gilroy-ExtraBold", size: 17))
-								Spacer()
-							}
-							.padding(16)
-							HStack {
-								Text("Episode: \(episode.ep), Season: \(episode.season)")
-									.font(Font.custom("Gilroy-Medium", size: 13))
-									.foregroundColor(Color.greenFuxia)
-								Spacer()
-								Text(episode.air_date)
-									.font(Font.custom("Gilroy-Medium", size: 13))
-									.foregroundColor(Color.secondaryTextColor)
-							}
-							.padding(.horizontal, 16)
-							.padding(.bottom, 16)
-						}
-					}
-					.cornerRadius(16)
-					.frame(height: 86)
+			if viewModel.isEpisodesEmpty() {
+				ProgressView()
 					.padding(24)
+			} else {
+				VStack (spacing: -24) {
+					ForEach(viewModel.episodes, id: \.name) { episode in
+						ZStack {
+							Color.groupColor
+							VStack {
+								HStack {
+									Text(episode.name)
+										.font(Font.custom("Gilroy-ExtraBold", size: 17))
+									Spacer()
+								}
+								.padding(16)
+								HStack {
+									Text("Episode: \(episode.ep), Season: \(episode.season)")
+										.font(Font.custom("Gilroy-Medium", size: 13))
+										.foregroundColor(Color.greenFuxia)
+									Spacer()
+									Text(episode.air_date)
+										.font(Font.custom("Gilroy-Medium", size: 13))
+										.foregroundColor(Color.secondaryTextColor)
+								}
+								.padding(.horizontal, 16)
+								.padding(.bottom, 16)
+							}
+						}
+						.cornerRadius(16)
+						.frame(height: 86)
+						.padding(24)
+					}
 				}
 			}
 		}
